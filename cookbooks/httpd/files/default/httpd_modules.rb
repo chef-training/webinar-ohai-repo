3d5659a461c85bc9e62b3bc77c0d7461ba271675
Franklin Webber <franklin.webber@gmail.com>
2017-01-30 17:50:40 -0600

    Removes the completed plugin
    Signed-off-by: Franklin Webber <franklin@chef.io>

--- cookbooks/httpd/files/default/httpd_modules.rb
+++
@@ -1,9 +0,0 @@
-Ohai.plugin :Apache do
-  provides 'apache/modules'
-
-  collect_data :default do
-    apache(Mash.new)
-    modules_cmd = shell_out('apachectl -t -D DUMP_MODULES')
-    apache[:modules] = modules_cmd.stdout
-  end
-end
